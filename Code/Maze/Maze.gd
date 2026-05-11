bc3726501968821af1a4da44099d98f3435598c0
unblinky <>
2026-05-11 16:44:35 -0700

    Created Room with Walls.

--- Code/Maze/Maze.gd
+++ Code/Maze/Maze.gd
@@ -1,9 +1,11 @@
 extends Node3D
 class_name Maze
 
+const ROOM = preload("res://Room/Room.tscn")
+
 
 func _ready() -> void:
-	pass # Replace with function body.
+	generate(5, 5)
 
 
 func _process(delta: float) -> void:
@@ -15,3 +17,16 @@ func _process(delta: float) -> void:
 		rotation_degrees.x -= 2.0
 	if Input.is_action_pressed("tilt_back"):
 		rotation_degrees.x += 2.0
+		#rotate(Vector3.FORWARD, 1.5)
+
+
+func generate(columns: int, rows: int):
+	var x_offset: float = (columns - 1) / 2.0
+	var z_offset: float = (rows - 1) / 2.0
+	
+	for row in rows:
+		for column in columns:
+			var room: Room = ROOM.instantiate()
+			room.position.x = column - x_offset
+			room.position.z = row - z_offset
+			add_child(room)
---
+++ Code/Room/Room.gd
@@ -0,0 +1,10 @@
+extends Node3D
+class_name Room
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
